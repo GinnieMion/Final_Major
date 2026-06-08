23d3d62c93b3dd3dd93b59fb9859dd7e6a1255d2
GinnieMion <virginiamay.delacruz.120@accesscreative.ac.uk>
2026-06-08 11:23:49 +0100

    added script

--- FMP/Assets/Flashlight.cs
+++ FMP/Assets/Flashlight.cs
@@ -1,21 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Flashlight : MonoBehaviour
 {
+    [SerializeField] private Image Batterybarsprite;
     public int charge = 0;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    public void UpdateHealthBar(int maxCharge, int currentCharge)
+    {
+        Batterybarsprite.fillAmount = currentCharge / maxCharge;
+    }
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             if(charge == 100)
             {
-                charge = 100;
+                charge = 0;
             }
             else
             {
